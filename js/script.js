0da80733dfc84809905cb59819a62faed589ab8c
enisabazi <enis.abazi@starlabspro.com>
2024-11-17 20:12:05 +0100

    wip./

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ const closeButton = document.querySelector('#closeButtonModalResponsive');
 const closeModal = document.querySelector('#closeModal');
 const fixed = document.querySelector('.fixed');
 const openModal = document.querySelector('#openModalButton');
-const redirect = document.querySelector('#redirectToLinkedin');
+const redirect = document.querySelector('#redirect');
 
-redirect.style.position = 'relative';
-redirect.style.zIndex = '10';
+redirect.addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevent other clicks from interfering
+    window.location.href = 'https://www.linkedin.com/in/enis-abazi-9a7097241/';
+});
 closeModal.style.display = 'none';
 fixed.style.position = 'inherit !important';
     fixed.style.zIndex = '-1';
